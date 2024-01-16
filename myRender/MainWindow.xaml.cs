87d180360ccc04d31c47a7bc3a6f4efe38ed55ba
380kkm <xzha0119@uni.sydney.edu.au>
2024-01-16 22:23:39 +1100

    class1

--- myRender/MainWindow.xaml.cs
+++ myRender/MainWindow.xaml.cs
@@ -19,9 +19,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        pac.Class1.DrawPoint(MyCanvas, 100, 100,10, 10, Colors.Red);
-        pac.Class1.DrawPoint(MyCanvas, 200, 200, 10, 10, Colors.Red);
-        pac.Class1.DrawLine_Attempt1(MyCanvas, Colors.Blue, new System.Numerics.Vector2(100, 100), new System.Numerics.Vector2(200, 200));
+        //pac.Class1.DrawPoint(MyCanvas, 100, 100,10, 10, Colors.Red);
+        //pac.Class1.DrawPoint(MyCanvas, 200, 200, 10, 10, Colors.Red);
+        pac.Class1.DrawLine(MyCanvas, Colors.Blue, new System.Numerics.Vector2(102, 202), new System.Numerics.Vector2(208, 0));
+        pac.Class1.DrawLine(MyCanvas, Colors.Black, new System.Numerics.Vector2(100, 300), new System.Numerics.Vector2(100, 100));
+        pac.Class1.DrawLine(MyCanvas, Colors.Yellow, new System.Numerics.Vector2(300, 100), new System.Numerics.Vector2(100, 50));
+        
+        
     }
     
 }
--- myRender/pac/Class1.cs
+++ myRender/pac/Class1.cs
@@ -7,15 +7,31 @@ using System.Numerics;
 public class Class1
 {
     //绘制点
+    #region MyRegion
     public static void DrawPoint(Canvas canvas, int x, int y, int xs = 1, int ys = 1, Color color = default) =>
         canvas.Children.Add(new Rectangle { Fill = new SolidColorBrush(color == default ? Colors.Black : color), Width = xs, Height = ys, Margin = new Thickness(x - xs / 2, y - ys / 2, 0, 0) });
-    //绘制线
-    public static void DrawLine_Attempt1(Canvas canvas, Color color = default, Vector2 p0 = default, Vector2 p1 = default)
+    #endregion
+     //绘制线
+    #region lineDrawing
+    /*
+    //逐点直线
+    public static void DrawLine_Attempt(Canvas canvas, Color color = default, Vector2 p0 = default, Vector2 p1 = default, float step = 0.01f)
     {
         if (p0 == default || p1 == default) return;
         color = color == default ? Colors.Black : color;
-        for (float t = 0; t < 1; t += 0.01f)
+        if (p0.X > p1.X) { (p0, p1) = (p1, p0); }
+        for (float t = 0; t < 1; t += step)
             DrawPoint(canvas, (int)(p0.X + (p1.X - p0.X) * t), (int)(p0.Y + (p1.Y - p0.Y) * t), color: color);
     }
+    */
+    //Bresenham算法
+    public static void DrawLine(Canvas canvas, Color color = default, Vector2 p0 = default, Vector2 p1 = default)
+    {
+        if (p0 == default || p1 == default) return;
+        color = color == default ? Colors.Black : color;
+        int x0 = (int)p0.X, y0 = (int)p0.Y, x1 = (int)p1.X, y1 = (int)p1.Y, dx = Math.Abs(x1 - x0), dy = Math.Abs(y1 - y0), sx = x0 < x1 ? 1 : -1, sy = y0 < y1 ? 1 : -1, err = dx - dy;
+        while (x0 != x1 || y0 != y1) { DrawPoint(canvas, x0, y0, color: color); int e2 = 2 * err; if (e2 > -dy) { err -= dy; x0 += sx; } if (e2 < dx) { err += dx; y0 += sy; } }
+    }
+    #endregion
     
 }
