d2865f1a63e54e6552e2d0b7b9730ca9166fc9d3
380kkm <xzha0119@uni.sydney.edu.au>
2024-03-01 21:19:46 +1100

    1

--- MyMath/obj/Debug/net8.0/MyMath.AssemblyInfo.cs
+++ MyMath/obj/Debug/net8.0/MyMath.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMath")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+280df42633a603efce15daeab65c62fcaa07bc87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMath")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMath")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
